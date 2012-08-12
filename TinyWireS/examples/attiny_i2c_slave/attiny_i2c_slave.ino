c832d2a4a9d1b584fbfd05f9a51ca8a75acb11fb
Eero af Heurlin <eero.afheurlin@iki.fi>
2012-08-12 19:00:21 +0300

    this proves the receive callback does not work yet...

--- TinyWireS/examples/attiny_i2c_slave/attiny_i2c_slave.ino
+++ TinyWireS/examples/attiny_i2c_slave/attiny_i2c_slave.ino
@@ -21,32 +21,35 @@ arduino pin 4 =     OC1B  = PORTB <- _BV(4) = SOIC pin 3 (Analog 2)
 volatile uint8_t i2c_regs[] =
 {
     10, // pwm
-    150, // blink on delay/4
-    150, // blink off delay/4
+    20, // blink on delay/4
+    200, // blink off delay/4
 };
 
-void setup()
+
+volatile byte reg_position;
+void requestEvent()
 {
-    // TODO: Tri-state this and wait for input voltage to stabilize 
-    pinMode(3, OUTPUT); // OC1B-, Arduino pin 3, ADC
-    digitalWrite(3, LOW); // Note that this makes the led turn on, it's wire this way to allow for the voltage sensing above.
+    TinyWireS.send(i2c_regs[reg_position]);
+    reg_position = (reg_position+1) % sizeof(i2c_regs);
+}
 
-    pinMode(1, OUTPUT); // OC1A, also The only HW-PWM -pin supported by the tiny core analogWrite
 
-    /**
-     * Reminder: taking care of pull-ups is the masters job
-     */
-
-    TinyWireS.begin(I2C_SLAVE_ADDRESS);
-
-    
-    // Whatever other setup routines ?
-    
+void blinkn(uint8_t blinks)
+{
     digitalWrite(3, HIGH);
+    while(blinks--)
+    {
+        digitalWrite(3, LOW);
+        delay(50);
+        digitalWrite(3, HIGH);
+        delay(100);
+    }
 }
 
 void receiveEvent(uint8_t howMany)
 {
+    blinkn(howMany);
+
     if (howMany < 1)
     {
         // Sanity-check
@@ -57,6 +60,9 @@ void receiveEvent(uint8_t howMany)
         // Also insane number
         return;
     }
+
+
+
     //TinyWireS.send(howMany);
     /*
     if (howMany < 2)
@@ -84,20 +90,49 @@ void receiveEvent(uint8_t howMany)
     }
 }
 
+
+void setup()
+{
+    // TODO: Tri-state this and wait for input voltage to stabilize 
+    pinMode(3, OUTPUT); // OC1B-, Arduino pin 3, ADC
+    digitalWrite(3, LOW); // Note that this makes the led turn on, it's wire this way to allow for the voltage sensing above.
+
+    pinMode(1, OUTPUT); // OC1A, also The only HW-PWM -pin supported by the tiny core analogWrite
+
+    /**
+     * Reminder: taking care of pull-ups is the masters job
+     */
+
+    TinyWireS.begin(I2C_SLAVE_ADDRESS);
+    TinyWireS.onReceive(receiveEvent);
+    TinyWireS.onRequest(requestEvent);
+
+    
+    // Whatever other setup routines ?
+    
+    digitalWrite(3, HIGH);
+}
+
 void loop()
 {
     // Poor-mans event handling (tinywire lib does not yet trigger the event right away), though I still wonder if we can still get two triggers during one I2C transaction (which will mess things up)
     uint8_t i2c_available = TinyWireS.available();
     if (i2c_available > 0)
     {
-        receiveEvent(i2c_available);
+        digitalWrite(3, LOW); // Note that this makes the led turn on, it's wire this way to allow for the voltage sensing above.
+        delay(500);
+        digitalWrite(3, HIGH); // Note that this makes the led turn on, it's wire this way to allow for the voltage sensing above.
+        delay(50);
+        blinkn(i2c_available);
     }
     analogWrite(1, i2c_regs[0]);
     i2c_regs[0] = i2c_regs[0]+10; // See if the loop is still runnign when I2C hangs
 
     // NOTE: this will also delay the receiving of the I2C instructions (since we do not have the onReceive/onRequest callback handling yet)
+    /*
     digitalWrite(3, LOW); // Note that this makes the led turn on, it's wire this way to allow for the voltage sensing above.
     delay(i2c_regs[1]*4);
     digitalWrite(3, HIGH);
     delay(i2c_regs[2]*4);
+    */
 }
